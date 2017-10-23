8031ac5dedce3e2d0d126d73dd28a5fa394be4be
ghybs <ghybs1@gmail.com>
2017-10-23 21:50:23 +0400

    Refactor(example): use manage-libs-versions, urijs and load-js-css
    so that it is much easier to demo and test with different Leaflet and Leaflet.markercluster versions, as well as MCG.Freezable versions.
    Also separated some setup / initialization and main page code into independent script files, for clarity.
    Use HTTPS for OSM tiles as well, even though the server looks to be currently unavailable…

---
+++ docs/examples/mcg-freezable-libs-setup.js
@@ -0,0 +1,96 @@
+// To be executed after manage-libs-versions is ready.
+// https://github.com/ghybs/manage-libs-versions
+// https://www.npmjs.com/package/manage-libs-versions
+var bundle1 = new manageLibsVersions.Bundle({
+	name: 'bundle1',
+	libs: [{
+		name: 'leaflet',
+		mandatory: true,
+		versions: [
+			_makeLeafletVersionAssets({
+				name: '1.2.0',
+				defaultVersion: true,
+				sriCss: 'sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ==',
+				sriSrcJs: 'sha512-YLT+I34kEPlk5OqR5XObf40B7sInrIU+bGe5VcwSpfR5OrFVjExFxfhVoJQEPZQWMyB53o3AU/bb5J91nc8CPA=='
+			}),
+			_makeLeafletVersionAssets({
+				name: '1.1.0',
+				sriCss: 'sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==',
+				sriSrcJs: 'sha512-sIPSXEX730B6EcdQyVPmIGp7f7ZrxIuECnkwYtPpEltG6NqOVtmBNoxHkMamNsAOHLMnDFaUoJYA4PWtzNZDuA=='
+			}),
+			_makeLeafletVersionAssets({ name: '1.0.3' }),
+			_makeLeafletVersionAssets({ name: '1.0.2' }),
+			_makeLeafletVersionAssets({ name: '1.0.1' }),
+			_makeLeafletVersionAssets({ name: '1.0.0' }),
+			_makeLeafletVersionAssets({ name: '0.7.7' })
+		]
+	}, {
+		name: 'leaflet.markercluster',
+		mandatory: true,
+		versions: [
+			_makeMCGVersionAssets({ name: '1.1.0', defaultVersion: true }),
+			_makeMCGVersionAssets({ name: '1.0.6' }),
+			_makeMCGVersionAssets({ name: '1.0.5' }),
+			_makeMCGVersionAssets({ name: '1.0.4' }),
+			_makeMCGVersionAssets({ name: '1.0.3' }),
+			_makeMCGVersionAssets({ name: '1.0.2' }),
+			_makeMCGVersionAssets({ name: '1.0.1' }),
+			_makeMCGVersionAssets({ name: '1.0.0' }),
+			_makeMCGVersionAssets({ name: '0.5.0' })
+		]
+	}, {
+		name: 'leaflet.markercluster.freezable',
+		mandatory: true,
+		versions: [
+			_makeFreezableVersionAssets({ name: '1.0.0' }),
+			_makeFreezableVersionAssets({ name: '0.1.1' }),
+			{
+				name: 'local',
+				defaultVersion: true,
+				assets: [{
+					type: 'script',
+					path: '../../dist/leaflet.markercluster.freezable-src.js'
+				}]
+			}
+		]
+	}]
+});
+
+function _makeLeafletVersionAssets(options) {
+	var versionName = options.name;
+
+	return {
+		name: versionName,
+		defaultVersion: options.defaultVersion,
+		assets: [
+			manageLibsVersions.makeStylesheet('https://unpkg.com/leaflet@{{VERSION}}/dist/leaflet.css', versionName, options.sriCss),
+			manageLibsVersions.makeScript('https://unpkg.com/leaflet@{{VERSION}}/dist/leaflet-src.js', versionName, options.sriSrcJs)
+		]
+	};
+}
+
+function _makeMCGVersionAssets(options) {
+	var versionName = options.name;
+
+	return {
+		name: versionName,
+		defaultVersion: options.defaultVersion,
+		assets: [
+			manageLibsVersions.makeStylesheet('https://unpkg.com/leaflet.markercluster@{{VERSION}}/dist/MarkerCluster.css', versionName, options.sriCss),
+			manageLibsVersions.makeStylesheet('https://unpkg.com/leaflet.markercluster@{{VERSION}}/dist/MarkerCluster.Default.css', versionName, options.sriDefaultCss),
+			manageLibsVersions.makeScript('https://unpkg.com/leaflet.markercluster@{{VERSION}}/dist/leaflet.markercluster-src.js', versionName, options.sriSrcJs)
+		]
+	};
+}
+
+function _makeFreezableVersionAssets(options) {
+	var versionName = options.name;
+
+	return {
+		name: versionName,
+		defaultVersion: options.defaultVersion,
+		assets: [
+			manageLibsVersions.makeScript('https://unpkg.com/leaflet.markercluster.freezable@{{VERSION}}/dist/leaflet.markercluster.freezable-src.js', versionName, options.sriSrcJs)
+		]
+	};
+}
---
+++ docs/examples/mcg-freezable.js
@@ -0,0 +1,135 @@
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 12,
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}),
+	latlng = L.latLng(48.86, 2.34);
+
+
+var map = L.map('map', {center: latlng, zoom: 11, layers: [tiles]});
+
+var mcg = L.markerClusterGroup();
+
+for (var i = 0; i < 50; i += 1) {
+	L.marker(getRandomLatLng()).addTo(mcg).bindPopup("Marker " + i);
+}
+
+mcg.freezeAtZoom();
+
+mcg.addTo(map);
+
+function getRandomLatLng() {
+	return [
+		48.86 + 0.1 * Math.random() - 0.05,
+		2.34 + 0.16 * Math.random() - 0.08
+	];
+}
+
+
+
+///////////////////////
+// Set-up buttons.
+///////////////////////
+
+onButtonClick("freezeAtCurrentZoomButton", function () {
+	mcg.freezeAtZoom();
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("freezeAtZoomButton", function () {
+	mcg.freezeAtZoom(parseInt(frozenZoomInput.value, 10));
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("freezeMaxKeepSpiderfyButton", function () {
+	mcg.freezeAtZoom("maxKeepSpiderfy");
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("freezeMaxButton", function () {
+	mcg.freezeAtZoom("max");
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("unfreezeButton", function () {
+	mcg.unfreeze();
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("disableKeepSpiderfyButton", function () {
+	mcg.disableClusteringKeepSpiderfy();
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("disableButton", function () {
+	mcg.disableClustering();
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("enableButton", function () {
+	mcg.enableClustering();
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("addMCGtoMap", function () {
+	map.addLayer(mcg);
+	updateCurrentFrozenZoom();
+});
+
+onButtonClick("removeMCGfromMap", function () {
+	map.removeLayer(mcg);
+});
+
+function onButtonClick(buttonId, callback) {
+	getById(buttonId).addEventListener("click", callback);
+}
+
+function getById(id) {
+	return document.getElementById(id);
+}
+
+// Range slider frozenZoomInput
+var frozenZoomInput = getById("frozenZoomInput"),
+	frozenZoomInputValue = getById("frozenZoomInputValue"),
+	currentFrozenZoom = getById("currentFrozenZoom");
+
+getById("frozenZoomInput").addEventListener("input", updateFrozenInputValue);
+
+updateFrozenInputValue();
+
+updateCurrentFrozenZoom();
+
+function updateFrozenInputValue() {
+	frozenZoomInputValue.innerHTML = frozenZoomInput.value;
+}
+
+function updateCurrentFrozenZoom() {
+	currentFrozenZoom.innerHTML = mcg._frozen
+		? mcg._zoom
+		: "<span class='italic'>(not frozen)</span>";
+}
+
+
+
+///////////////////////
+// Show map zoom
+///////////////////////
+
+var currentMapZoom = getById("currentMapZoom");
+
+map.on("zoomend", showMapCurrentZoom);
+
+showMapCurrentZoom();
+
+function showMapCurrentZoom() {
+	currentMapZoom.innerHTML = map.getZoom();
+}
+
+
+
+///////////////////////
+// Patch MCG from CDN
+///////////////////////
+
+// No longer need to patch, now that PR Leaflet/Leaflet.markercluster#607 has been merged
+// into v0.5.0 and v1.0.0+
+// See https://github.com/Leaflet/Leaflet.markercluster/pull/607
