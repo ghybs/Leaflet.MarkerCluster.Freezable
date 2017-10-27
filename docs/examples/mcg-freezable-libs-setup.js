99b55bbeab23c97897a381698e5f850643358759
ghybs <ghybs1@gmail.com>
2017-10-27 12:46:30 +0400

    Feat(demo): add SRI for Leaflet 0.7.7 - 1.0.3
    taken from Leaflet repo (1.0.3) and from jsDelivr (0.7.7 - 1.0.2).

--- docs/examples/mcg-freezable-libs-setup.js
+++ docs/examples/mcg-freezable-libs-setup.js
@@ -26,11 +26,31 @@ var bundle1 = new manageLibsVersions.Bundle({
 				sriCss: 'sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==',
 				sriSrcJs: 'sha512-sIPSXEX730B6EcdQyVPmIGp7f7ZrxIuECnkwYtPpEltG6NqOVtmBNoxHkMamNsAOHLMnDFaUoJYA4PWtzNZDuA=='
 			}),
-			_makeLeafletVersionAssets({ name: '1.0.3' }),
-			_makeLeafletVersionAssets({ name: '1.0.2' }),
-			_makeLeafletVersionAssets({ name: '1.0.1' }),
-			_makeLeafletVersionAssets({ name: '1.0.0' }),
-			_makeLeafletVersionAssets({ name: '0.7.7' })
+			_makeLeafletVersionAssets({
+				name: '1.0.3',
+				sriCss: 'sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ==',
+				sriSrcJs: 'sha512-WXoSHqw/t26DszhdMhOXOkI7qCiv5QWXhH9R7CgvgZMHz1ImlkVQ3uNsiQKu5wwbbxtPzFXd1hK4tzno2VqhpA=='
+			}),
+			_makeLeafletVersionAssets({
+				name: '1.0.2',
+				sriCss: 'sha256-9mfj77orHLh2GsN7CbMvpjO/Wny/ZZhR7Pu7hy0Yig4=',
+				sriSrcJs: 'sha256-VHkU4jC5FoqC9jHC5YP77Btx6zcwBBoE3v5e2UWExeM='
+			}),
+			_makeLeafletVersionAssets({
+				name: '1.0.1',
+				sriCss: 'sha256-B5Af7a59HZfTwOlorb6pQSEj6l8I3jj785fgpPiLnx4=',
+				sriSrcJs: 'sha256-TKXcBJJSGl/FfgsHsWSqI/8eI2CVTs+/Z2xlBjnYh4Q='
+			}),
+			_makeLeafletVersionAssets({
+				name: '1.0.0',
+				sriCss: 'sha256-PFO9CfQJUoeHCST/XnUtF/RaNuH/LgguGQ1ykt3iA7Y=',
+				sriSrcJs: 'sha256-Gga8lgup9173iOt2e8DKISTUSbz+JK6510SjbXoGcsQ='
+			}),
+			_makeLeafletVersionAssets({
+				name: '0.7.7',
+				sriCss: 'sha256-ymZGho+WjeQQ2jvjHInYJd0h20DI6/AE0fYq+BGYXqY=',
+				sriSrcJs: 'sha256-nYr14YrqqLUvH8oPD82ha4p6ZIZ4emL9VGggPHRTnsE='
+			})
 		]
 	}, {
 		name: 'leaflet.markercluster',
